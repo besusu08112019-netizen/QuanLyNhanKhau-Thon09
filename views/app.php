fb122629408447e313b81180293fe8915ef88b86
besusu08112019-netizen <besusu08112019@gmail.com>
2026-06-28 12:22:11 +0700

    Load Sprint 3 session guard

--- views/app.php
+++ views/app.php
@@ -168,5 +168,6 @@
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <script src="assets/js/app.js"></script>
+  <script src="assets/js/session.js"></script>
 </body>
 </html>
