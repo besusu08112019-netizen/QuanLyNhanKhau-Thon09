57cb84353420b514b39e8b168976e7e368ddc71a
besusu08112019-netizen <besusu08112019@gmail.com>
2026-06-28 12:41:07 +0700

    Add backup controller

---
+++ app/Controllers/BackupController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\BaseController;
+use App\Models\Backup;
+
+final class BackupController extends BaseController
+{
+    private Backup $backups;
+
+    public function __construct($request)
+    {
+        parent::__construct($request);
+        $this->backups = new Backup();
+    }
+
+    public function index(): void
+    {
+        $this->requirePermission('backup', 'read');
+        $this->ok($this->backups->page($this->query()));
+    }
+
+    public function create(): void
+    {
+        $user = $this->requirePermission('backup', 'export');
+        $backup = $this->backups->createSqlDump((int) $user['id']);
+        $this->audit($user, 'backup', 'export', 'Tạo bản sao lưu SQL', null, ['fileName' => $backup['fileName'], 'size' => $backup['size'], 'checksum' => $backup['checksum']]);
+        header('Content-Type: application/sql; charset=utf-8');
+        header('Content-Disposition: attachment; filename="' . $backup['fileName'] . '"');
+        echo $backup['content'];
+        exit;
+    }
+}
