baeb137ee24fa113407f7b7e8890313262821209
besusu08112019-netizen <besusu08112019@gmail.com>
2026-06-28 13:19:27 +0700

    Add permission management controller

---
+++ app/Controllers/PermissionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\BaseController;
+use App\Models\Permission;
+
+final class PermissionController extends BaseController
+{
+    private Permission $permissions;
+
+    public function __construct($request)
+    {
+        parent::__construct($request);
+        $this->permissions = new Permission();
+    }
+
+    public function index(): void
+    {
+        $this->requirePermission('permission', 'read');
+        $this->ok($this->permissions->matrix());
+    }
+
+    public function update(): void
+    {
+        $user = $this->requirePermission('permission', 'update');
+        $matrix = $this->permissions->updateMany((array) $this->input('items', []), (int) $user['id']);
+        $this->audit($user, 'permission', 'update', 'Cập nhật phân quyền');
+        $this->ok($matrix);
+    }
+}
